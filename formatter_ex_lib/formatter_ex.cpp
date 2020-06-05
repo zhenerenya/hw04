a0014c26699300e2e8c6c89ba67a7252a464c567
zhenerenya <zhenyachmchk@gmail.com>
2020-06-05 11:02:12 +0300

    task 3

--- formatter_ex_lib/formatter_ex.cpp
+++ formatter_ex_lib/formatter_ex.cpp
@@ -1,5 +1,5 @@
 #include "formatter_ex.hpp"
-#include "../formatter_lib/formatter.hpp"
+#include "formatter.hpp"
 
 std::ostream& formatter_ex(std::ostream& stream, const std::string& str){
 	return stream<<formatter(str);
