a0ff97f6c96384f2699315771190c2ee9560a353
zhenerenya <zhenyachmchk@gmail.com>
2020-06-06 17:34:34 +0300

    edit  solver_lib/solver.cpp

--- solver_lib/solver.cpp
+++ solver_lib/solver.cpp
@@ -11,6 +11,6 @@ void solve(float a, float b, float c, float& x1, float& x2)
         throw std::logic_error{"error: discriminant < 0"};
     }
 
-    x1 = (-b - std::sqrtf(d)) / (2 * a);
-    x2 = (-b + std::sqrtf(d)) / (2 * a);
+    x1 = (-b - sqrtf(d)) / (2 * a);
+    x2 = (-b + sqrtf(d)) / (2 * a);
 }
